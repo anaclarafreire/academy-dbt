95d8849505294fe8b25d8d6354e6886751c7ab2d
anaclarafreire <anaclaraf1306@gmail.com>
2025-01-29 18:15:22 -0300

    Reapply "avanços 2 dias depois"
    This reverts commit ef55456b553362c68d2202f7a57ce0b660098d43.

--- models/staging/stg_address.sql
+++ models/staging/stg_address.sql
@@ -1,10 +1,13 @@
 with source_address as (
     select
-        cast(a.addressid as int64) as id_endereco, 
-        cast(a.city as string) as nome_cidade,
-        cast(a.stateprovinceid as int64) as id_estado
+        cast(a.addressid as int64) as pk_id_endereco
+        , cast(a.addressline1 as string) as linha_endereco_1
+        , cast(a.city as string) as nome_cidade
+        , cast(a.stateprovinceid as int64) as fk_id_estado
     from {{ source('RAW_sap_aw', 'address') }} a
 )
 
 select *
 from source_address
+
+
